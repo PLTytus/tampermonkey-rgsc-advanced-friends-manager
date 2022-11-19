0f7978539e8c2530c8848b84832a52edb2711de9
Rafał 'Tytus' Miętkiewski <rafal.tytus98@gmail.com>
2022-11-19 17:46:56 +0100

    v2.4.0 - meta

--- rgsc_advanced_friends_manager.meta.js
+++ rgsc_advanced_friends_manager.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            RGSC Advanced Friends Manager
 // @author          PLTytus
-// @version         2.3.6
+// @version         2.4.0
 // @namespace       http://gtaweb.eu/tampermonkey
 // @downloadURL     https://bitbucket.org/PLTytus/rgsc-advanced-friends-manager/raw/master/rgsc_advanced_friends_manager.user.js
 // @updateURL       https://bitbucket.org/PLTytus/rgsc-advanced-friends-manager/raw/master/rgsc_advanced_friends_manager.meta.js
@@ -11,6 +11,7 @@
 // @resource        CSS2 https://bitbucket.org/PLTytus/rgsc-advanced-friends-manager/raw/master/src/rgsc_advanced_friends_manager.css
 // @grant           GM_getResourceText
 // @grant           GM_addStyle
+// @grant           GM_xmlhttpRequest
 // @require         http://code.jquery.com/jquery-1.12.4.min.js
 // @require         http://code.jquery.com/ui/1.12.1/jquery-ui.js
 // ==/UserScript==
