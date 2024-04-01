aa0b435413ca94d36150c8c198e53c1838507523
PLTytus <rafal.tytus98@gmail.com>
2024-04-01 18:48:27 +0200

    moved to github

--- rgsc_advanced_friends_manager.user.js
+++ rgsc_advanced_friends_manager.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name            RGSC Advanced Friends Manager
 // @author          PLTytus
-// @version         2.4.3
+// @version         2.4.4
 // @namespace       http://gtaweb.eu/tampermonkey
-// @downloadURL     https://bitbucket.org/PLTytus/rgsc-advanced-friends-manager/raw/master/rgsc_advanced_friends_manager.user.js
-// @updateURL       https://bitbucket.org/PLTytus/rgsc-advanced-friends-manager/raw/master/rgsc_advanced_friends_manager.meta.js
+// @downloadURL     https://github.com/PLTytus/tampermonkey-rgsc-advanced-friends-manager/raw/master/rgsc_advanced_friends_manager.user.js
+// @updateURL       https://github.com/PLTytus/tampermonkey-rgsc-advanced-friends-manager/raw/master/rgsc_advanced_friends_manager.meta.js
 // @match           https://*.socialclub.rockstargames.com/*
 // @match           https://socialclub.rockstargames.com/*
 // @resource        CSS1 https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css
-// @resource        CSS2 https://bitbucket.org/PLTytus/rgsc-advanced-friends-manager/raw/master/src/rgsc_advanced_friends_manager.css
+// @resource        CSS2 https://raw.githubusercontent.com/PLTytus/tampermonkey-rgsc-advanced-friends-manager/master/src/rgsc_advanced_friends_manager.css
 // @grant           GM_getResourceText
 // @grant           GM_addStyle
 // @grant           GM_xmlhttpRequest
